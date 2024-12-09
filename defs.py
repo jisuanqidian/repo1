ee0f8caf7354e203b392d9162481c0ad50fb46df
jisuanqidian <838125810@qq.com>
2024-12-09 16:03:34 +0800

    Update defs.py

--- defs.py
+++ defs.py
@@ -1,29 +1,48 @@
 import os
 import re
+import sys
 
 def get_workpath() :
     workpath = os.getcwd()
     if os.path.isdir(workpath+'/env') != True and os.path.isdir(workpath+'/cfg') != True and os.path.isdir(workpath+'/th') != True:
         print('Warning: must work on module directory!')
-        quit()
+        sys.exit()
     else :
         workpath = workpath.split('/sim')[0]
         os.environ['WORKPATH'] = workpath
     return workpath
 
-def check_keyword_in_file(filename,keywords=['Error','UVM_ERROR','UVM_FATAL'],skip_lines_num=10) :
+def check_keyword_in_file(filename,keywords=[r'^\bError\b','UVM_ERROR','UVM_FATAL','offending'],skip_lines_num=10) :
     try :
         with open(filename, mode = 'r') as file :
             for skip in range(skip_lines_num) :
                 next(file)
             for line in file :
-                if 'UVM Report Summary' in line :
+                if re.search('UVM Report.*Summary',line) != None :
                     return 0
                 if any(re.search(text,line,re.IGNORECASE) for text in keywords) :
                     return 1
         return 0
     except Exception as e :
         print(e)
+        return 1
+
+def rename_hist_files(path, file_name):
+    if not os.path.exists(path):
+        print("The specified path does not exist.")
+        return
+    pattern = re.compile(r'^' + re.escape(file_name) + r'\.(\d+)$')
+    file_ind_list = [int(i.split('.')[-1]) for i in os.listdir(path) if pattern.match(i) != None]
+    file_ind_list.sort(reverse=True)
+    ori_file = os.path.join(path, f'{file_name}')
+    for ind,i in zip(range(len(file_ind_list)-1,0-1,-1),file_ind_list):
+        if(ind == i and os.path.exists(ori_file)):
+            old_file = os.path.join(path, f'{file_name}.{i}')
+            new_file = os.path.join(path, f'{file_name}.{i+1}')
+            os.rename(old_file,new_file)
+    if(os.path.exists(ori_file)):
+        new_file = os.path.join(path, f'{file_name}.0')
+        os.rename(ori_file,new_file)
 
 if __name__ == '__main__' :
     print(check_keyword_in_file('test.txt',['^error']))
