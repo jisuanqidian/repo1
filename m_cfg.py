c516717c86168e0ae57f36876934ebc35abb46e0
jisuanqidian <838125810@qq.com>
2024-12-09 16:22:09 +0800

    Update m_cfg.py

--- m_cfg.py
+++ m_cfg.py
@@ -2,10 +2,16 @@ import sys
 sys.path.append('/home/qiaochuli/bin/xsim')
 from cfg import *
 
+com_opts['base'] = ''
+com_opts['base'] += ''
+
 caseName.append('adder_sanity_test')
 
 for i in range(1,101):
-    caseName.append('addr_test_{:04d}'.format(i))
+    cn = 'test_{02d}'.format(i)
+    depends[cn] = 'base'
+    caseName.append(cn)
+    if i == 1 : sim_opts[cn] = ' +UVM_TESTNAME= '
 
 if __name__ == '__main__' :
     print(caseName)
